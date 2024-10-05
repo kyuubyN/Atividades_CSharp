29472ad6e186ce7248275f1633ce5f96e4311413
Kaue Oliveira <102341451+kyuubyN@users.noreply.github.com>
2024-10-05 05:19:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = 2;
+
+if (a == 3) {
+    Console.WriteLine("Parabens");
+} else {
+    Console.WriteLine("Sem parabens");
+}
